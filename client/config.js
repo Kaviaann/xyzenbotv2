2e8bdfb8bc5541cf4f372c786cd028289dda4c25
xyzendev <adriangota2007@gmail.com>
2024-04-01 16:58:51 +0700

    @xyzendev/xyzenbotv2

---
+++ client/config.js
@@ -0,0 +1,12 @@
+export default {
+    owners: [],
+    number: {
+        type: "BOT",
+        country: "ID",
+        number: "16319311234"
+    },
+    usePairingCode: true,
+    prefix: ".",
+    write_store: false,
+    self: true
+}
---
+++ xyzen.config.js
@@ -0,0 +1,7 @@
+export default {
+    name: "xyzen",
+    version: "1.0.0",
+    description: "A simple whatsapp bot",
+    licensi: "FREE PLAN",
+    key: "70a51766-4d11-477f-94d7-e5405a714e3a"
+}
